95091db4de5565bab4abad9597ecf3d2473e77f2
Frederick Cabedoce <frederick.cabedoce@hotmail.com>
2023-04-15 16:36:21 -0400

    Spring initializr commit

---
+++ src/main/java/cabedoce/cloud/solutions/ratingjava17/RatingJava17Application.java
@@ -0,0 +1,13 @@
+package cabedoce.cloud.solutions.ratingjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatingJava17Application.class, args);
+	}
+
+}
---
+++ src/test/java/cabedoce/cloud/solutions/ratingjava17/RatingJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package cabedoce.cloud.solutions.ratingjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
